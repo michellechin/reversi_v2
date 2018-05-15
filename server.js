ee95d095402c8a2110f02207165b50537f09b58c
Chin, Michelle <mdchin@uci.edu>
2018-05-14 23:07:56 -0700

    Update port to 8080

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var directory = __dirname + '/public';
 /* If we aren't on Heroku, then we need to readjust the port and directory information and we know that because port won't be set*/
 if(typeof port == 'undefined' || !port) {
 	directory = './public';
-	port = 8081;	
+	port = 8080;	
 }
 
 /* Set up a static web-server that will deliver files from the filesystem */
