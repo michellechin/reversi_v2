8078addf5de80ae9fc864266bd330cec199049e4
Chin, Michelle <mdchin@uci.edu>
2018-05-27 14:24:41 -0700

    lobby and main.js file updates

---
+++ public/main.js
@@ -0,0 +1,23 @@
+/* functions for general use */
+
+/* This function returns the value associated with 'whichParam' on the URL */
+
+function getURLParameters(urlParams)
+{
+	var pageURL = window.location.search.substring(1);
+	var pageURLVariables = pageURL.split('&');
+	for(var i = 0; i < pageURLVariables.length; i++){
+		var parameterName = pageURLVariables[i].split('=');
+		if(parameterName[0] == whichParam){
+			return parameterName[1];
+		}
+	}
+}
+
+var username = getURLParameters('username')
+if('undefined' == typeof username || !username){
+	username = 'Anonymous_'+Math.random();
+}
+
+
+$('#messages').append('<h4>'+username+('username')+'</h4>');
